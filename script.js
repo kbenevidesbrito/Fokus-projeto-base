2917897b34c80fd698cf5572d6781b6b87e35313
kbenevidesbrito <kbenevidesbrito@gmail.com>
2026-02-23 17:53:09 -0300

    atualiza lógica de controle de foco e descanso, adiciona mensagens dinâmicas e corrige seleção de botão ativo

--- script.js
+++ script.js
@@ -6,23 +6,50 @@ const botaoIniciar = document.querySelector('.app__card-primary-button')
 const banner = document.querySelector('.app__image')
 const displayTempo = document.querySelector('#timer')
 const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
 const duracaoFoco = 1500; 
 const duracaoDescansoCurto = 300; 
 const duracaoDescansoLongo = 900; 
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
+    focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 })
 
 function alterarContexto(contexto) {
+    botoes.forEach(function (contexto){
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+            <strong class='app__title-strong'>mergulhe no que importa.</strong>
+            `
+            break;
+        case 'descanso-curto':
+            titulo.innerHTML = `
+            Que tal dar uma respirada? <strong class='app__title-strong'>Faça uma pausa curta!</strong>
+            `
+            break;
+        case 'descanso-longo':
+            titulo.innerHTML = `
+            Hora de voltar à superfície. <strong class='app__title-strong'>Faça uma pausa longa.</strong>
+            `
+            break;
+        default:
+            break;
+    }
 }
