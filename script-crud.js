af30ea9988f04f865b4c7fcf5c25823a2b5995dd
kbenevidesbrito <kbenevidesbrito@gmail.com>
2026-02-25 19:02:54 -0300

    adiciona funcionalidade de edição de tarefas e destaca tarefa selecionada

--- script-crud.js
+++ script-crud.js
@@ -2,8 +2,10 @@ const botaoAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textarea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+let tarefaSelecionada = null
 
 function atualizarTarefas() {
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
@@ -30,9 +32,12 @@ function criarElementoTarefa(tarefa) {
 
     botao.onclick = () => {
         const novaDescricao = prompt('Editar tarefa', tarefa.descricao)
-        paragrafo.textContent = novaDescricao
-        tarefa.descricao = novaDescricao
-        atualizarTarefas()
+        console.log('Nova descrição da tarefa:', novaDescricao)
+        if (novaDescricao) {
+            paragrafo.textContent = novaDescricao
+            tarefa.descricao = novaDescricao
+            atualizarTarefas()
+        }
     }
 
     const imgBotao = document.createElement('img')
@@ -40,6 +45,21 @@ function criarElementoTarefa(tarefa) {
     botao.append(imgBotao)
 
     li.append(svg, paragrafo, botao)
+
+    li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active')
+            .forEach(elemento => {
+                elemento.classList.remove('app__section-task-list-item-active')
+        })
+        if (tarefaSelecionada === tarefa) {
+            paragrafoDescricaoTarefa.textContent = ''
+            tarefaSelecionada = null
+            return
+        }
+        tarefaSelecionada = tarefa
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao
+        li.classList.add('app__section-task-list-item-active')
+    }
     return li
 }
 
